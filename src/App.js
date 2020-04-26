73a49c0b10c229eb5550531b7b07d7cdad0a6f9a
John Herbener <john@herbener.cloud>
2020-04-26 14:03:27 -0400

    web socket (chat) setup

--- src/App.js
+++ src/App.js
@@ -3,33 +3,62 @@ import logo from './logo.svg';
 
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/login"
-            render={(props) => {
-              return <h1>Login</h1>;
-            }}
-          />
+import * as ChatActions from './store/actions/chatActions';
 
-          <Route
-            path="/"
-            render={(props) => {
-              return <h1>Root</h1>;
-            }}
-          />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.setupSocket();
+  }
+  render() {
+    return (
+      <div className="App">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+
+            if (this.props.socket) {
+              this.props.socket.send(
+                JSON.stringify({
+                  type: 'Hello',
+                  data: 'World',
+                })
+              );
+            }
+          }}
+        >
+          Send Message
+        </button>
+
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/login"
+              render={(props) => {
+                return <h1>Login</h1>;
+              }}
+            />
+
+            <Route
+              path="/"
+              render={(props) => {
+                return <h1>Root</h1>;
+              }}
+            />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => ({
   ...state.auth,
+  ...state.chat,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  setupSocket: () => {
+    dispatch(ChatActions.setupSocket());
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
